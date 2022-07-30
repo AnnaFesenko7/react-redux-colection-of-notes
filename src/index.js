421ce71ee0f358dd96a2030da21ec39c5a22f1cd
AnnaFesenko7 <annafecenko7@gmail.com>
2022-07-30 12:04:23 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -10,13 +10,13 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(
   document.getElementById('root')
 ).render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    {/* <PersistGate loading="...Loading" persistor={persistor}> */}
-    <BrowserRouter basename="/react-redux-colection-of-notes/">
-      <App />
-    </BrowserRouter>
-    {/* </PersistGate> */}
-  </Provider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      {/* <PersistGate loading="...Loading" persistor={persistor}> */}
+      <BrowserRouter basename="/react-redux-colection-of-notes/">
+        <App />
+      </BrowserRouter>
+      {/* </PersistGate> */}
+    </Provider>
+  </React.StrictMode>
 );
